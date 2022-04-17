13d2aab893c4bfab690db1d6aea9985389168594
Liam James <liam.james@ymail.com>
2022-04-16 19:26:12 -0500

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,13 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+#include "lib/utf8.hpp"
+
+int main(int argc, char** argv) {
+	utf8::cout << "Input: ";
+
+	utf8::string test;
+	utf8::cin >> test;
+
+	utf8::cout << "Output: " << test;
+}
